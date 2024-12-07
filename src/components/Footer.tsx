b8bef5a46db0f72304a70b33ef18a6d7180ca46b
persipica <likekhs0107@naver.com>
2024-12-07 21:23:52 +0900

    1st

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,9 +9,7 @@ const Footer: React.FC = () => {
         <hr className="my-6 border-blueGray-600" />
         <div className="flex flex-wrap text-left lg:text-left">
           <div className="w-full lg:w-6/12 px-4">
-            <h4 className="text-3xl font-semibold text-blueGray-700">
-              Old Emporium
-            </h4>
+            <h4 className="text-3xl font-semibold text-blueGray-700">JBnara</h4>
             <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
               좋은 상품을 합리적인 가격에 만나보세요!
             </h5>
@@ -82,7 +80,7 @@ const Footer: React.FC = () => {
           <div className="w-full md:w-4/12 px-4 mx-auto text-center">
             <div className="text-sm text-blueGray-500 font-semibold py-1">
               Copyright © <span id="get-current-year">2024</span>
-              Teamfinalmarket
+              JBnara
             </div>
           </div>
         </div>
